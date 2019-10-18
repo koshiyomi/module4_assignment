8dfe54a61636503caa0fa96ff6b5fa43cf2577b1
kevin <mashirowhite333@gmail.com>
2019-10-18 04:56:17 -0400

    update

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,43 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingList')
-.config(RoutesConfig);
+angular.module('MenuApp').config(RoutesConfig);
 
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
 function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-  // Redirect to home page if no other URL matches
-  $urlRouterProvider.otherwise('/');
-
-  // *** Set up UI states ***
-  $stateProvider
-
-  // Home page
-  .state('home', {
-    url: '/',
-    templateUrl: 'src/shoppinglist/templates/home.template.html'
-  })
-
-  // Premade list page
-  .state('mainList', {
-    url: '/main-list',
-    templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
-    controller: 'MainShoppingListController as mainList',
-    resolve: {
-      items: ['ShoppingListService', function (ShoppingListService) {
-        return ShoppingListService.getItems();
-      }]
-    }
-  })
-
-  .state('mainList.itemDetail', {
-    url: '/item-detail/{itemId}',
-    templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
-    controller: "ItemDetailController as itemDetail"
-  });
-
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'src/templates/home.template.html'
+        })
+        .state('categories', {
+            url: '/categories',
+            templateUrl: 'src/templates/categories.template.html',
+            controller: 'categoriesController as categoriesCtrl',
+            resolve: {
+                categories: ['MenuDataService', function (MenuDataService) {
+                    return MenuDataService.getAllCategories();
+                }]
+            }
+        })
+        .state('items', {
+            url: '/items/{categoryShortName}',
+            templateUrl: 'src/templates/items.template.html',
+            controller: 'itemsController as itemsCtrl',
+            params: {
+                categoryShortName: null,
+                categoryName: null
+            },
+            resolve: {
+                items: ['$stateParams', 'MenuDataService',
+                    function ($stateParams, MenuDataService) {
+                    return MenuDataService
+                        .getItemsForCategory($stateParams.categoryShortName);
+                }]
+            }
+        });
 }
-
 })();
